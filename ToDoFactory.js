7e272addcfd4db32a053233923d487978d74d6df
emilio <emsmnndz@gmail.com>
2016-05-31 09:47:29 +0100

    first commit

---
+++ ToDoFactory.js
@@ -0,0 +1,7 @@
+toDoApp.factory('ToDoFactory', function(){
+  var ToDo = function(ToDoText){
+    this.text = todoText;
+    this.completed = (typeof completed !== 'undefined') ? completed : false;
+  };
+  return Todo;
+});
