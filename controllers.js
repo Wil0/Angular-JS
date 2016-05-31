6c0565979dddf594640274b4422b6488af50c011
emilio <emsmnndz@gmail.com>
2016-05-31 14:44:51 +0100

    todos can be set as complete

--- controllers.js
+++ controllers.js
@@ -1,18 +1,18 @@
-toDoApp.controller('ToDoController', function() {
+toDoApp.controller('ToDoController', ['ToDoFactory', function(ToDoFactory) {
   // Save this to a variable as this can change depending on the context of
   // where it is being called, see
   // https://github.com/johnpapa/angular-styleguide/tree/master/a1#style-y032
   var self = this;
 
-  self.todos = [{ text: "ToDo1", completed: true }, { text: "ToDo2", completed: false }];
+  self.todos = [];
 
   self.addToDo = function(todoText) {
     // now, no matter what, we'll push new ToDos
     // to the controller.
-    self.todos.push({ text: todoText, completed: false });
+    self.todos.push(new ToDoFactory(todoText));
   };
 
   self.removeToDo = function() {
     self.todos.pop();
   };
-});
+}]);
