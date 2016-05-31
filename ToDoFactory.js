28eb84d7949e442e0fde6405f589f659f4c7c141
emilio <emsmnndz@gmail.com>
2016-05-31 14:10:46 +0100

    todos can be added and removed

--- ToDoFactory.js
+++ ToDoFactory.js
@@ -1,7 +1,10 @@
 toDoApp.factory('ToDoFactory', function(){
-  var ToDo = function(ToDoText){
+  var Todo = function(todoText){
     this.text = todoText;
     this.completed = (typeof completed !== 'undefined') ? completed : false;
+  };
+  Todo.prototype.complete = function() {
+    this.completed = true;
   };
   return Todo;
 });
--- controllers.js
+++ controllers.js
@@ -1,13 +1,18 @@
-toDoApp.controller('ToDoController', ['ToDoFactory', function(ToDoFactory) {
+toDoApp.controller('ToDoController', function() {
+  // Save this to a variable as this can change depending on the context of
+  // where it is being called, see
+  // https://github.com/johnpapa/angular-styleguide/tree/master/a1#style-y032
   var self = this;
 
-  self.todos = [];
+  self.todos = [{ text: "ToDo1", completed: true }, { text: "ToDo2", completed: false }];
 
   self.addToDo = function(todoText) {
-    self.todos.push(new ToDoFactory(todoText));
+    // now, no matter what, we'll push new ToDos
+    // to the controller.
+    self.todos.push({ text: todoText, completed: false });
   };
 
   self.removeToDo = function() {
     self.todos.pop();
   };
-}]);
+});
